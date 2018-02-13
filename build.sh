54ec5330c215d0d8a3ede89821082ab349d508ba
Auston Stewart <auston@shedbuilt.net>
2018-02-13 07:29:05 -1000

    Fixing compilation without OpenGL

--- build.sh
+++ build.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+./autogen.sh && \
 ./configure --prefix=/usr/local \
             --without-docs \
             --without-man \
             --with-gl=no && \
 # Fix broken no-GL support
 sed -i "/^DEFS = / s/\$/ -D__NO_OPENGL/" Makefile && \
+patch -Np1 -i "${SHED_PATCHDIR}/remove_duplicate_SDL_opengl_include.patch" && \
 make -j $SHED_NUMJOBS && \
 make DESTDIR="$SHED_FAKEROOT" install
