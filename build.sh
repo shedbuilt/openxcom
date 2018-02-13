072875ec267aeb42ffdf305bc7c1202a3f6431dd
Auston Stewart <auston@shedbuilt.net>
2018-02-12 22:51:25 -1000

    Fixing build without OpenGL

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-mkdir -pv build
-cd build
-cmake -DCMAKE_BUILD_TYPE=Release .. && \
+./configure --prefix=/usr/local \
+            --without-docs \
+            --without-man \
+            --with-gl=no && \
+# Fix broken no-GL support
+sed -i "/^DEFS = / s/\$/ -D__NO_OPENGL/" Makefile && \
 make -j $SHED_NUMJOBS && \
 make DESTDIR="$SHED_FAKEROOT" install
