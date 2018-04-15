c3a82bd96ac579c55d25ed7c9ea42ea70145a08a
Auston Stewart <auston@shedbuilt.net>
2018-04-15 12:14:24 -1000

    Updating packaging for blank

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@
             --with-gl=no &&
 # Fix broken no-GL support
 sed -i "/^DEFS = / s/\$/ -D__NO_OPENGL/" Makefile &&
-patch -Np1 -i "${SHED_PATCHDIR}/remove_duplicate_SDL_opengl_include.patch" &&
-make -j $SHED_NUMJOBS &&
-make DESTDIR="$SHED_FAKEROOT" install
+patch -Np1 -i "${SHED_PKG_PATCH_DIR}/remove_duplicate_SDL_opengl_include.patch" &&
+make -j $SHED_NUM_JOBS &&
+make DESTDIR="$SHED_FAKE_ROOT" install
