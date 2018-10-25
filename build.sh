725bdcf3b4702d63457829238d210353fddc625a
Auston Stewart <auston@shedbuilt.net>
2018-10-25 08:06:28 -1000

    Attempting to get a working non-OpenGL build

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ declare -A SHED_PKG_LOCAL_OPTIONS=${SHED_PKG_OPTIONS_ASSOC}
 if [ -z "${SHED_PKG_LOCAL_OPTIONS[gl]}" ]; then
     # Fix broken no-GL support
     patch -Np1 -i "${SHED_PKG_PATCH_DIR}/remove_duplicate_SDL_opengl_include.patch" &&
-    sed -i "s/^AX_CHECK_GL/AX_CHECK_GL(,\[AC_DEFINE(\[__NO_OPENGL\], \[1\], \[Defined if no valid OpenGL implementation is found.\])\])/" configure.ac || exit 1
+    patch -Np1 -i "${SHED_PKG_PATCH_DIR}/openxcom-allow-no-opengl-config.patch" || exit 1
 fi
 ./autogen.sh &&
 ./configure --prefix=/usr/local \
