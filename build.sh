7b151f8d7e888adbc1b8ae194e48686372aad945
Auston Stewart <auston@shedbuilt.net>
2018-10-24 18:26:16 -1000

    Fixing the non-OpenGL config

--- build.sh
+++ build.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
+declare -A SHED_PKG_LOCAL_OPTIONS=${SHED_PKG_OPTIONS_ASSOC}
+# Configure
+if [ -z "${SHED_PKG_LOCAL_OPTIONS[gl]}" ]; then
+    # Fix broken no-GL support
+    patch -Np1 -i "${SHED_PKG_PATCH_DIR}/remove_duplicate_SDL_opengl_include.patch" &&
+    sed -i "s/^AX_CHECK_GL/AX_CHECK_GL(,\[AC_DEFINE(\[__NO_OPENGL\], \[1\], \[Defined if no valid OpenGL implementation is found.\])\])/" configure.ac || exit 1
+fi
 ./autogen.sh &&
 ./configure --prefix=/usr/local \
             --without-docs \
-            --without-man \
-            --with-gl=no &&
-# Fix broken no-GL support
-sed -i "/^DEFS = / s/\$/ -D__NO_OPENGL/" Makefile &&
-patch -Np1 -i "${SHED_PKG_PATCH_DIR}/remove_duplicate_SDL_opengl_include.patch" &&
+            --without-man &&
 make -j $SHED_NUM_JOBS &&
 make DESTDIR="$SHED_FAKE_ROOT" install
